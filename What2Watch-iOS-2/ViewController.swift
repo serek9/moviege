e067c20f0120efeeca273da4eb40c7175b57ed58
sergiodiaz <sergiodiaz808@gmail.com>
2017-01-29 17:00:14 +0100

    Initial Commit

---
+++ What2Watch-iOS-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What2Watch-iOS-2
+//
+//  Created by Sergio Díaz on 29/1/17.
+//  Copyright © 2017 Sergio Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ What2Watch-iOS-2Tests/What2Watch_iOS_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  What2Watch_iOS_2Tests.swift
+//  What2Watch-iOS-2Tests
+//
+//  Created by Sergio Díaz on 29/1/17.
+//  Copyright © 2017 Sergio Díaz. All rights reserved.
+//
+
+import XCTest
+@testable import What2Watch_iOS_2
+
+class What2Watch_iOS_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
